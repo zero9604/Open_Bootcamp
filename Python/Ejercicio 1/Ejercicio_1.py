32e4ca1a0c62d5f4744de22ac0dd62eaf19108d4
Mateo Arango <matarango96@gmail.com>
2023-05-02 00:19:23 -0500

    Se inicia el modulo de python y el ejercicio 1

---
+++ Python/Ejercicio 1/Ejercicio_1.py
@@ -0,0 +1,5 @@
+"""Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+Tienes que subir capturas de pantalla en una carpeta comprimida zip."""
+
+tmp = "Hola mundo!"
+print(tmp)
