9e58984f001fce94a16198ff1f33fd4e44e050ab
Mateo Arango <matarango96@gmail.com>
2023-05-02 10:44:27 -0500

    Se actualiza el ejercicio #2 y captura

--- Python/Ejercicio 2/Ejercicio_2.py
+++ Python/Ejercicio 2/Ejercicio_2.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+"""Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+Tienes que subir capturas de pantalla en una carpeta comprimida zip."""
+
+tmp = "Hola mundo!"
+print(tmp)
+tmp = "Esto es una modificación"
+print(tmp)
