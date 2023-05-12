0106c9f12bcfe8f2ac07d3c50ebdc4208d58f8be
Mateo Arango <matarango96@gmail.com>
2023-05-12 11:04:51 -0500

    se modifica un comentario

--- Python/Ejercicio 5/Ejercicio_5.py
+++ Python/Ejercicio 5/Ejercicio_5.py
@@ -3,7 +3,7 @@
 def biciesto(año):
     a = False
     if año % 4 == 0:        
-        if año % 100 == 0 and año % 400 ==0:          # Verificamos si el año es secular
+        if año % 100 == 0 and año % 400 ==0:  # Verificamos si el año es secular  y divisible entre 400
             return "Año biciesto (366 días)"
         
         elif año % 100 !=0:
