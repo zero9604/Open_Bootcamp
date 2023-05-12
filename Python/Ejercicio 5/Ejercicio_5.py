25dc68928c8377cb1b41e71e7cab23540e16bf82
Mateo Arango <matarango96@gmail.com>
2023-05-12 11:14:17 -0500

    Correción ortografica

--- Python/Ejercicio 5/Ejercicio_5.py
+++ Python/Ejercicio 5/Ejercicio_5.py
@@ -1,13 +1,13 @@
 "Escribe una función que pueda decirte si un año (número entero) es bisiesto o no."
 
-def biciesto(año):
+def bisiesto(año):
 
     # Verificamos si el año es divisible por 4, secular  y divisible entre 400
     if año % 4 == 0 and (año % 100 !=0 or año %400 ==0):        
 
-        return "Año biciesto (366 días)"
-    return "Año no biciesto (365 días)"
+        return "Año bisiesto (366 días)"
+    return "Año no bisiesto (365 días)"
     
 a = int(input("Por favor ingrese un año: \n"))
-print(biciesto(a))
+print(bisiesto(a))
 
