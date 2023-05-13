475e0bd8209d284404a97792c084eb4d62cf11a5
Mateo Arango <matarango96@gmail.com>
2023-05-13 01:26:31 -0500

    Creamos el archivo para el ejercicio #2

---
+++ Python/Ejercicio 6/Ejercicio_6.2.py
@@ -0,0 +1,4 @@
+"""En este segundo ejercicio, tendréis que crear un programa que tenga una clase llamada Alumno que tenga 
+como atributos su nombre y su nota. Deberéis de definir los métodos para inicializar sus atributos, 
+imprimirlos y mostrar un mensaje con el resultado de la nota y si ha aprobado o no."""
+
