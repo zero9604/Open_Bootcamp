4c54147d0a414f316ffcb7157c3fe896987e8aca
Mateo Arango <matarango96@gmail.com>
2023-05-13 01:22:40 -0500

    Terminamos el ejercicio

--- Python/Ejercicio 6/Ejercicio_6.1.py
+++ Python/Ejercicio 6/Ejercicio_6.1.py
@@ -13,33 +13,33 @@ Por otro lado crearéis la clase Coche la cual heredará de Vehículo y tendrá 
 Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.
 """
 
-
+# Creamos la clase padre
 class vehiculo:
-    color = ''
-    ruedas = 0
-    puertas = ''
-
+    
+    # Inicializamos los atributos
     def __init__(self, color, ruedas, puertas):
         self.color = color
         self.ruedas = ruedas
         self.puertas = puertas
     
-
+# Creamos la clase hija
 class coche(vehiculo):
-    velocidad = 0
-    cilindrada = 0
 
+    # inicializamos los atributos
     def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
         super().__init__(color,ruedas,puertas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
+    
+    # Creamos el siguiente metodo para la visualización de la clase por medio de un print
+    def __str__(self):
+        return "color {}, {} km/h, {} ruedas, {} puertas, {} cc".format( self.color, self.velocidad, self.ruedas, self.puertas, self.cilindrada )
 
-deportivo = coche('negro',4,3,50,125)
-print(deportivo.color)
-print(deportivo.ruedas)
-print(deportivo.puertas)
-print(deportivo.velocidad)
-print(deportivo.cilindrada)
+#--------------------------------------------Programa principal-----------------------------------------#
+
+# Instanciamos e imprimimos el objeto "deportivo"
+deportivo = coche('negro',4,3,200,125)
+print(deportivo)
 
 
 
