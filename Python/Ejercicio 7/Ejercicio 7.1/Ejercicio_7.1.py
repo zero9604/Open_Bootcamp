82cfb2c1cbaba26b2fba40191ada7a1c4d21454b
Mateo Arango <matarango96@gmail.com>
2023-05-13 22:50:49 -0500

    Creamos y terminamos el ejercicio 7.1

---
+++ Python/Ejercicio 7/Ejercicio 7.1/Ejercicio_7.1.py
@@ -0,0 +1,20 @@
+"""
+En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora:
+sumar, restar, multiplicar y dividir.
+
+Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas.
+Los resultados tenéis que mostrarlos por consola.
+"""
+
+# Importamos el modulo calculadora
+import calculadora
+
+n1 = float(input("Por favor ingrese un primer valor: "))
+n2 = float(input("Por favor ingrese un primer valor: "))
+
+print(f"la suma de ambos números es: {calculadora.sumar(n1,n2)}")
+print(f"la resta de ambos números es: {calculadora.restar(n1,n2)}")
+print(f"la multiplicación de ambos números es: {calculadora.multiplicar(n1,n2)}")
+print(f"la división de ambos números es: {calculadora.dividir(n1,n2)}")
+
+
