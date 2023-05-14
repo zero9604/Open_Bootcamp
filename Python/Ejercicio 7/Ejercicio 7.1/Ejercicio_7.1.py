cbf21af6f2ae7440708db843c0aa8e39498354d7
Mateo Arango <matarango96@gmail.com>
2023-05-13 22:51:45 -0500

    Se comenta el codigo

--- Python/Ejercicio 7/Ejercicio 7.1/Ejercicio_7.1.py
+++ Python/Ejercicio 7/Ejercicio 7.1/Ejercicio_7.1.py
@@ -9,9 +9,11 @@ Los resultados tenéis que mostrarlos por consola.
 # Importamos el modulo calculadora
 import calculadora
 
+# Solicitamos al usuario dos números flotantes
 n1 = float(input("Por favor ingrese un primer valor: "))
 n2 = float(input("Por favor ingrese un primer valor: "))
 
+# Imprimimos las operaciones realizadas con ambos números
 print(f"la suma de ambos números es: {calculadora.sumar(n1,n2)}")
 print(f"la resta de ambos números es: {calculadora.restar(n1,n2)}")
 print(f"la multiplicación de ambos números es: {calculadora.multiplicar(n1,n2)}")
